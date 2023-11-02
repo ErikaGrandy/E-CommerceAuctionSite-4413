2013d0e8484af5e6d21702b5e6ccf615040ae9f3
suhas <suhas@Suha-Dell.home>
2023-11-01 21:22:29 -0400

    made adjustments to User.java and Address.java, made db locally, started UserService

--- BidSphere/src/main/java/com/yorku/BidSphere/User/User.java
+++ BidSphere/src/main/java/com/yorku/BidSphere/User/User.java
@@ -2,9 +2,80 @@ package com.yorku.BidSphere.User;
 
 public class User {
 
-	int userID;
-	String firstName;
-	String lastName;
-	String email;
-	Address address;
+	private String userName;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private Address address;
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
--- BidSphere/src/main/java/com/yorku/BidSphere/User/UserService.java
+++ BidSphere/src/main/java/com/yorku/BidSphere/User/UserService.java
@@ -1,5 +1,46 @@
 package com.yorku.BidSphere.User;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserService {
+
+	public List<User> readAll() {
+		String sql = "SELECT userName, password, firstName, lastName, streetAddress, streetNumber, city, province, postalCode, country FROM users";
+
+		List<User> users = new ArrayList<>();
+
+		try (Connection conn = DatabaseConnection.connect();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)) {
+
+			while (rs.next()) {
+				User user = new User();
+				Address address = new Address();
+				user.setUserName(rs.getString("userName"));
+				user.setPassword(rs.getString("password"));
+				user.setFirstName(rs.getString("firstName"));
+				user.setLastName(rs.getString("lastName"));
+				address.setStreetAddress(rs.getString("streetAddress"));
+				address.setStreetNumber(rs.getInt("streetNumber"));
+				address.setCity(rs.getString("city"));
+				address.setProvince(rs.getString("province"));
+				address.setPostalCode(rs.getString("postalCode"));
+				address.setCountry(rs.getString("country"));
+
+				user.setAddress(address);
+				
+				users.add(user);
+
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		return users;
+
+	}
 
 }
