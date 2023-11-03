f65aad180809ecb9ecdd2833f487d28625a81242
suhasid098 <suhasid098@gmail.com>
2023-11-02 23:30:55 -0400

    calling the UserService done minus testing

--- BidSphere/src/main/java/com/yorku/BidSphere/User/UserService.java
+++ BidSphere/src/main/java/com/yorku/BidSphere/User/UserService.java
@@ -30,7 +30,7 @@ public class UserService {
 				address.setCountry(rs.getString("country"));
 
 				user.setAddress(address);
-				
+
 				users.add(user);
 
 			}
@@ -41,6 +41,98 @@ public class UserService {
 		}
 		return users;
 
+	}
+
+	public void create(User user) {
+		String sql = "INSERT INTO users(userName, password, firstName, lastName, streetAddress, streetNumber, city, province, postalCode, country) VALUES(?,?,?,?,?,?,?,?,?,?)";
+
+		try (Connection conn = DatabaseConnection.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, user.getUserName());
+			pstmt.setString(2, user.getPassword());
+			pstmt.setString(3, user.getFirstName());
+			pstmt.setString(4, user.getLastName());
+			pstmt.setString(5, user.getAddress().getStreetAddress());
+			pstmt.setInt(6, user.getAddress().getStreetNumber());
+			pstmt.setString(7, user.getAddress().getCity());
+			pstmt.setString(8, user.getAddress().getProvince());
+			pstmt.setString(9, user.getAddress().getPostalCode());
+			pstmt.setString(10, user.getAddress().getCountry());
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public User read(String userName) {
+		String sql = "SELECT userName, password, firstName, lastName, streetAddress, streetNumber, city, province, postalCode, country FROM users WHERE userName = ?";
+		User user = null;
+		try (Connection conn = DatabaseConnection.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			// Set the corresponding parameter
+			pstmt.setString(1, userName);
+			// Execute the query and get the result set
+			try (ResultSet rs = pstmt.executeQuery()) {
+				// Check if a result was returned
+				if (rs.next()) {
+					user = new User();
+					Address address = new Address();
+					// set the properties of the student object
+					user.setUserName(userName);
+					user.setPassword(rs.getString("password"));
+					user.setFirstName(rs.getString("firstName"));
+					user.setLastName(rs.getString("lastName"));
+					address.setStreetAddress(rs.getString("streetAddress"));
+					address.setStreetNumber(rs.getInt("streetNumber"));
+					address.setCity(rs.getString("city"));
+					address.setProvince(rs.getString("province"));
+					address.setPostalCode(rs.getString("postalCode"));
+					address.setCountry(rs.getString("country"));
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		return user;
+	}
+
+	public void update(String userName, User user) {
+		String sql = "UPDATE users SET password = ?, firstName = ?, lastName = ?, streetAddress = ?, streetNumber = ?, city =?, province = ?, postalCode = ?, country = ? WHERE userName = ?";
+
+		try (Connection conn = DatabaseConnection.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			// Set the corresponding parameters
+			pstmt.setString(1, user.getUserName());
+			pstmt.setString(2, user.getPassword());
+			pstmt.setString(3, user.getFirstName());
+			pstmt.setString(4, user.getLastName());
+			pstmt.setString(5, user.getAddress().getStreetAddress());
+			pstmt.setInt(6, user.getAddress().getStreetNumber());
+			pstmt.setString(7, user.getAddress().getCity());
+			pstmt.setString(8, user.getAddress().getProvince());
+			pstmt.setString(9, user.getAddress().getPostalCode());
+			pstmt.setString(10, user.getAddress().getCountry());
+
+			// Update the student record
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void delete(String userName) {
+		String sql = "DELETE FROM users WHERE userName = ?";
+
+		try (Connection conn = DatabaseConnection.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			// Set the corresponding parameter
+			pstmt.setString(1, userName);
+			// Delete the student record
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
