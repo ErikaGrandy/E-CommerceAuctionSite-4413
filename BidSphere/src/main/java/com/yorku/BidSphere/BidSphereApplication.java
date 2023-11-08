f4352ecc859175a4348eded504ebeb632797219f
Erika Grandy <36168874+ErikaGrandy@users.noreply.github.com>
2023-11-08 13:29:23 -0500

    Added JPA to Project, Modified Payment Object

--- BidSphere/src/main/java/com/yorku/BidSphere/BidSphereApplication.java
+++ BidSphere/src/main/java/com/yorku/BidSphere/BidSphereApplication.java
@@ -2,8 +2,11 @@ package com.yorku.BidSphere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories({"com.yorku.BidSphere.Payment", "com.yorku.BidSphere.Bid", 
+	"com.yorku.BidSphere.Catalog", "com.yorku.BidSphere.User"})
 public class BidSphereApplication {
 
 	public static void main(String[] args) {
--- BidSphere/src/main/java/com/yorku/BidSphere/Payment/PaymentService.java
+++ BidSphere/src/main/java/com/yorku/BidSphere/Payment/PaymentService.java
@@ -1,5 +1,14 @@
 package com.yorku.BidSphere.Payment;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class PaymentService {
 
+	@Autowired private PaymentRepository paymentRepository;
+
+	protected String createPayment(Payment payment)
+	{
+		int paymentID = paymentRepository.save(payment).getPaymentID();
+		return "Payment has been saved with an ID of " + paymentID;
+	}
 }
