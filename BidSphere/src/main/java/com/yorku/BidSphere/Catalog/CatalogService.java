d33d0ab0b4e379543ab32380e181203bf388bb46
Erika Grandy <36168874+ErikaGrandy@users.noreply.github.com>
2023-11-08 19:39:14 -0500

    Small Autowire Changes

--- BidSphere/src/main/java/com/yorku/BidSphere/Catalog/CatalogService.java
+++ BidSphere/src/main/java/com/yorku/BidSphere/Catalog/CatalogService.java
@@ -2,75 +2,17 @@ package com.yorku.BidSphere.Catalog;
 
 import com.yorku.BidSphere.Payment.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Optional;
 
+@Service
 public class CatalogService {
 
 
-    private CatalogItemRepository repo = new CatalogItemRepository() {
-        @Override
-        public <S extends CatalogItem> S save(S entity) {
-            return null;
-        }
-
-        @Override
-        public <S extends CatalogItem> Iterable<S> saveAll(Iterable<S> entities) {
-            return null;
-        }
-
-        @Override
-        public Optional<CatalogItem> findById(Integer integer) {
-            return Optional.empty();
-        }
-
-        @Override
-        public boolean existsById(Integer integer) {
-            return false;
-        }
-
-        @Override
-        public Iterable<CatalogItem> findAll() {
-            return null;
-        }
-
-        @Override
-        public Iterable<CatalogItem> findAllById(Iterable<Integer> integers) {
-            return null;
-        }
-
-        @Override
-        public long count() {
-            return 0;
-        }
-
-        @Override
-        public void deleteById(Integer integer) {
-
-        }
-
-        @Override
-        public void delete(CatalogItem entity) {
-
-        }
-
-        @Override
-        public void deleteAllById(Iterable<? extends Integer> integers) {
-
-        }
-
-        @Override
-        public void deleteAll(Iterable<? extends CatalogItem> entities) {
-
-        }
-
-        @Override
-        public void deleteAll() {
-
-        }
-    };
+    @Autowired private CatalogItemRepository repo;
 
     public ArrayList<CatalogItem> getAllItems()
     {
--- BidSphere/src/main/java/com/yorku/BidSphere/Payment/PaymentRepository.java
+++ BidSphere/src/main/java/com/yorku/BidSphere/Payment/PaymentRepository.java
@@ -1,9 +1,11 @@
 package com.yorku.BidSphere.Payment;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 //AUTO-IMPLEMENTED
 //WILL INCLUDE BASIC CRUD
+@Repository
 public interface PaymentRepository extends CrudRepository<Payment, Integer> {
 	//Leave empty, unless adding in custom SQL queries.
 }
